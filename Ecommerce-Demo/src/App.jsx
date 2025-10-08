06f990ec9761900a6e3b9b0b8c30f9e71366ad87
YouTube Downloader <youtube.downloader@example.com>
2025-10-08 16:12:52 +0530

    split header into component and use link

--- Ecommerce-Demo/src/App.jsx
+++ Ecommerce-Demo/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <Routes>
-      <Route index element={<HomePage />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/checkout" element={<CheckoutPage />} />
       <Route path="/orders" element={<OrdersPage />} />
       <Route path="/tracking" element={<TrackingPage />} />
