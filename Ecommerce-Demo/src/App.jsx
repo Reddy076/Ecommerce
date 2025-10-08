9c819f94bd8a0abd65d82e7e2acde6a1853316ce
YouTube Downloader <youtube.downloader@example.com>
2025-10-08 15:37:39 +0530

    added a checkout page

--- Ecommerce-Demo/src/App.jsx
+++ Ecommerce-Demo/src/App.jsx
@@ -1,6 +1,8 @@
 import {Routes, Route} from 'react-router';
 
-import { HomePage } from './Pages/HomePage'
+import { HomePage } from './Pages/HomePage';
+
+import { CheckoutPage } from './Pages/CheckoutPage';
 
 import './App.css'
 
@@ -9,6 +11,7 @@ function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
       
     </Routes>
   )
