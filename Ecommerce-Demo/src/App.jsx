dc54ad258d726414d8c981eff28de607f1f8c5b8
YouTube Downloader <youtube.downloader@example.com>
2025-10-08 14:28:59 +0530

    changes to react router

--- Ecommerce-Demo/src/App.jsx
+++ Ecommerce-Demo/src/App.jsx
@@ -8,12 +8,8 @@ function App() {
 
   return (
     <Routes>
-      <Route>
-        path="/" element={<HomePage />}
-      </Route>
+      <Route path="/" element={<HomePage />} />
     </Routes>
-    
-    
   )
 }
 
