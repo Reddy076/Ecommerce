9fbd3badedf0851247c467a76ffe1bc9078b3bfe
YouTube Downloader <youtube.downloader@example.com>
2025-10-10 17:44:54 +0530

    added delete button interactive

--- Ecommerce-Demo/src/App.jsx
+++ Ecommerce-Demo/src/App.jsx
@@ -1,42 +1,28 @@
-import { Routes, Route } from 'react-router-dom';
-
-import { useEffect, useState } from 'react';
-
-import { HomePage } from './Pages/home/HomePage';
-
 import axios from 'axios';
-
-import { CheckoutPage } from './Pages/checkout/CheckoutPage';
-
-import { OrdersPage } from './Pages/orders/OrdersPage';
-
-import { TrackingPage } from './Pages/TrackingPage';
-
+import { Routes, Route } from 'react-router';
+import { useState, useEffect } from 'react';
+import { HomePage } from './pages/home/HomePage';
+import { CheckoutPage } from './pages/checkout/CheckoutPage';
+import { OrdersPage } from './pages/orders/OrdersPage';
 import './App.css'
 
 function App() {
   const [cart, setCart] = useState([]);
 
   const loadCart = async () => {
-      const response = await axios.get('/api/cart-items?expand=product')
-      setCart(response.data);
-    };
+    const response = await axios.get('/api/cart-items?expand=product');
+    setCart(response.data);
+  };
 
   useEffect(() => {
-    
     loadCart();
-
   }, []);
-
-
 
   return (
     <Routes>
-      <Route path="/" element={<HomePage cart={cart}  loadCart={loadCart}/>} />
-      <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
-      <Route path="/orders" element={<OrdersPage cart={cart} />} />
-      <Route path="/tracking" element={<TrackingPage />} />
-
+      <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} loadCart={loadCart} />} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
     </Routes>
   )
 }
