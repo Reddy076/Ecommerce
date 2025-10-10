96632974ac909e612b8993f3b2aa98f02203e313
YouTube Downloader <youtube.downloader@example.com>
2025-10-10 12:01:32 +0530

    Get cart from backend and display cart quantity

--- Ecommerce-Demo/src/Pages/HomePage.jsx
+++ Ecommerce-Demo/src/Pages/HomePage.jsx
@@ -7,11 +7,17 @@ import './HomePage.css';
 export function HomePage() {
 
   const [products, setProducts]=useState([]);
+  const [cart, setCart]=useState([]);
 
   useEffect(()=>{
     axios.get('http://localhost:3000/api/products')
   .then((response)=>{
       setProducts(response.data)
+    });
+    axios.get('http://localhost:3000/api/cart-items')
+    .then((response)=>{
+      console.log(response.data);
+      setCart(response.data);
     });
   },[]);
 
@@ -22,7 +28,7 @@ export function HomePage() {
     <>
       <title>Ecommerce</title>
 
-      <Header />
+      <Header cart={cart}/>
       <div className="home-page">
         <div className="products-grid">
           {products.map((product) => {
